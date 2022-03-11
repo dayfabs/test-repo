4eba4585f7023e5f1ac469b61b8db2bc7ca72319
Temidayo Fabayo <dayofabayo@navapbc.com>
2022-03-11 14:25:17 -0500

    Modifications to Tf file

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-my first file
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
