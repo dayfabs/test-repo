fe87dca02db8247d43f9e6cd03569c7d730e5cef
Temidayo Fabayo <dayofabayo@navapbc.com>
2022-03-11 15:02:24 -0500

    added other subnets

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "vpc" {
   azs             = var.vpc_azs
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
+  other_subnets  = var.vpc_other_subnets
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
 
